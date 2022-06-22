57eab08f39c388b8d20eccfee9e1289771bc6c53
goyalritik <ritikg6776@gmail.com>
2022-06-22 22:21:50 +0530

    upto route

--- components/SideBar.js
+++ components/SideBar.js
@@ -3,36 +3,36 @@ const SideBar = () => {
   return (
     <div>
 
-<div class="relative bg-white dark:bg-gray-800">
-    <div class="flex flex-col sm:flex-row sm:justify-around">
-        <div class="w-72 h-screen">
-            <nav class="mt-10 px-6 ">
-                <a class="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg " href="#">
-                    <span class="mx-4 text-lg font-normal">
+<div className="relative bg-white dark:bg-gray-800">
+    <div className="flex flex-col sm:flex-row sm:justify-around">
+        <div className="w-72 h-screen">
+            <nav className="mt-10 px-6 ">
+                <a className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg " href="#">
+                    <span className="mx-4 text-lg font-normal">
                         Element
                     </span>
-                    <span class="flex-grow text-right">
+                    <span className="flex-grow text-right">
                     </span>
                 </a>
-                <a class="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-800 dark:text-gray-100 rounded-lg bg-gray-100 dark:bg-gray-600" href="#">
-                    <span class="mx-4 text-lg font-normal">
+                <a className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-800 dark:text-gray-100 rounded-lg bg-gray-100 dark:bg-gray-600" href="#">
+                    <span className="mx-4 text-lg font-normal">
                         Form
                     </span>
-                    <span class="flex-grow text-right">
+                    <span className="flex-grow text-right">
                     </span>
                 </a>
-                <a class="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg " href="#">
-                    <span class="mx-4 text-lg font-normal">
+                <a className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg " href="#">
+                    <span className="mx-4 text-lg font-normal">
                         Commerce
                     </span>
-                    <span class="flex-grow text-right">
+                    <span className="flex-grow text-right">
                     </span>
                 </a>
-                <a class="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg " href="#">
-                    <span class="mx-4 text-lg font-normal">
+                <a className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg " href="#">
+                    <span className="mx-4 text-lg font-normal">
                         Navigation
                     </span>
-                    <span class="flex-grow text-right">
+                    <span className="flex-grow text-right">
                     </span>
                 </a>
             </nav>
